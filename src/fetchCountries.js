7195eedad927c49bd02095c68b7ab20f78654d58
Iryna Gumeniuk <iriwkagumenuk@gmail.com>
2021-08-21 20:09:08 +0200

    js methods

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,9 +1,4 @@
-import { alert, notice, info, success, error } from '@pnotify/core/dist/PNotify';
-import '@pnotify/core/dist/BrightTheme.css';
-
 export default function fetchCountries(searchQuery) {
     return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
-        .then(r => r.json())
-        .catch(alert({ text: "Ошибка HTTP: " + r.status }))
-}
-
+        .then(response => response.json());
+};
